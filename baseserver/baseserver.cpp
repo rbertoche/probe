9ab632109c32abaff1a2142e654854e1b116115e
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2021-05-29 06:06:54 -0300

    Adiciona lib baseserver pra maximizar reuso de código

---
+++ baseserver/baseserver.cpp
@@ -0,0 +1,79 @@
+
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <iostream>
+
+#include "baseserver.h"
+
+Receiver::Receiver(boost::asio::io_service& io_service,
+		   const boost::asio::ip::address& multicast_address,
+		   const boost::asio::ip::address& listen_address)
+	: socket_(io_service)
+{
+	std::cerr.flush();
+	// Create the socket so that multiple may be bound to the same address.
+	ip::udp::endpoint listen_endpoint(listen_address, multicast_port);
+	std::cerr.flush();
+	socket_.open(listen_endpoint.protocol());
+	std::cerr.flush();
+	socket_.set_option(ip::udp::socket::reuse_address(true));
+	std::cerr.flush();
+
+	// Join the multicast group.
+	ip::multicast::join_group option(multicast_address);
+	boost::system::error_code ec;
+	socket_.set_option( option, ec);
+	if (listen_address != unspecified){
+		socket_.bind(listen_endpoint);
+		std::cerr << "Ouvindo em " << listen_address << ":" << multicast_port << std::endl;
+	}
+
+	socket_.async_receive_from( buffer(data_, max_length), sender_endpoint_,
+				    boost::bind(&Receiver::handle_receive_from, this,
+						placeholders::error,
+						placeholders::bytes_transferred));
+	std::cerr << "Ouvindo MC em " << multicast_address << ":" << multicast_port<< std::endl;
+}
+
+
+void Receiver::handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd)
+{
+	if (!error)
+	{
+		std::cout.write(data_, bytes_recvd);
+		std::cout << std::endl;
+
+		socket_.async_receive_from( buffer(data_, max_length), sender_endpoint_,
+					    boost::bind(&Receiver::handle_receive_from, this,
+							placeholders::error,
+							placeholders::bytes_transferred));
+
+		respond(sender_endpoint_);
+	}
+}
+
+
+void BaseServer::handle_send_to(const boost::system::error_code& error)
+{
+	if (!error && message_count_ < max_message_count)
+	{
+		timer_.expires_from_now(boost::posix_time::seconds(1));
+		timer_.async_wait( boost::bind(&BaseServer::handle_timeout, this,
+					       placeholders::error));
+	}
+}
+
+
+void BaseServer::handle_timeout(const boost::system::error_code& error)
+{
+	if (!error)
+	{
+		std::ostringstream os;
+		os << "Message " << message_count_++;
+		message_ = os.str();
+
+		socket_.async_send_to( boost::asio::buffer(message_), endpoint_,
+				       boost::bind(&BaseServer::handle_send_to, this,
+						   boost::asio::placeholders::error));
+	}
+}
---
+++ baseserver/baseserver.h
@@ -0,0 +1,72 @@
+#ifndef BASESERVER_H
+#define BASESERVER_H
+
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <boost/asio.hpp>
+
+
+const short multicast_port = 30001;
+
+const int max_message_count = 10;
+
+using namespace boost::asio;
+
+
+class Receiver
+{
+protected:
+	Receiver(io_service& io_service,
+		 const ip::address& multicast_address,
+		 const ip::address& listen_address);
+
+	void handle_receive_from(const boost::system::error_code& error,
+				 size_t bytes_recvd);
+
+	virtual void respond(ip::udp::endpoint sender) = 0;
+
+private:
+	ip::udp::endpoint sender_endpoint_;
+	enum { max_length = 1024 };
+	char data_[max_length];
+protected:
+	ip::udp::socket socket_;
+
+
+};
+
+const ip::address unspecified(ip::address::from_string("255.255.255.255"));
+
+
+
+class BaseServer
+	: public Receiver
+{
+protected:
+	BaseServer(io_service &io_service,
+		   const ip::address& multicast_address,
+		   const ip::address& listen_address=unspecified)
+	: Receiver(io_service,
+		   multicast_address,
+		   listen_address)
+	, timer_(io_service)
+	, message_count_(0)
+	, endpoint_(multicast_address, multicast_port)
+	{
+	}
+
+	virtual void respond(ip::udp::endpoint sender) = 0;
+
+	void handle_send_to(const boost::system::error_code& error);
+
+
+	void handle_timeout(const boost::system::error_code& error);
+
+	deadline_timer timer_;
+	int message_count_;
+	std::string message_;
+	ip::udp::endpoint endpoint_;
+};
+#endif // BASESERVER_H
