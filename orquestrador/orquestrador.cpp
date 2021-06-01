25141f6e1d7499ecc5d2b1bf0dde3bd7bbfed8bc
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2021-06-01 13:50:52 -0300

    implementa o orquestrador

--- orquestrador/orquestrador.cpp
+++ orquestrador/orquestrador.cpp
@@ -13,47 +13,160 @@
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 
 #include "basemulticastserver.h"
+#include "mensagem.h"
+#include "dump.h"
 
 using namespace std;
 using namespace boost::asio;
 
-class Servidor :
+class Orquestrador :
 	public BaseMulticastServer
 {
 public:
-	Servidor(io_service &io_service,
+	Orquestrador(io_service &io_service,
 	       const ip::address& multicast_address,
-	       unsigned short port)
+	       unsigned short multicast_port)
 	: BaseMulticastServer(io_service,
 		     multicast_address,
-		     port)
-	{}
+		     multicast_port)
+	, mc_endpoint(multicast_address, multicast_port)
+	, exito_servidor(false)
+	, exito_cliente(false)
+	, desliga_servidor(false)
+	, desliga_cliente(false)
+	, ack_servidor(false)
+	, ack_cliente(false)
+	{
+	}
+
+	ip::udp::endpoint mc_endpoint;
+	static const Origem origem_local = ORQUESTRADOR;
+
+	bool exito_servidor;
+	bool exito_cliente;
+	bool desliga_servidor;
+	bool desliga_cliente;
+	bool ack_servidor;
+	bool ack_cliente;
 
 	void start(){
-		vector<unsigned char> dummy(10);
-		respond(endpoint_,
-			dummy);
-
+		exito_servidor = false;
+		exito_cliente = false;
+		ack_servidor = false;
+		ack_cliente = false;
+		Mensagem m(DISPARA, origem_local, 1, 1);
+		send_to(socket_, Mensagem::pack(m), mc_endpoint);
 	}
 
 	virtual void respond(ip::udp::endpoint sender,
 			     const vector<unsigned char>& data){
 
 		ostringstream os;
-		os << "Message " << message_count_++ << ": ";
-
-		for (vector<unsigned char>::const_iterator it = data.begin();
-		     it < data.end();
-		     it++){
-			os << setw(2) << hex << unsigned(*it) << " ";
-		}
-		os << "FIM" << endl;
-		os << "recipient: " << sender << endl;
+#ifdef DEBUG_1
+		os << "Message " << message_count_++ << " ";
+		os << "recipient: " << origin << endl;
 		message_ = os.str();
 		cerr << message_;
+		dump(data);
+#endif
 
-		send_to(socket_, data, sender);
+		Mensagem m(Mensagem::unpack(data));
+		if (m.origem() == origem_local){
+			// Ignora
+		} else if (m.tipo() == ECO){
+			cerr << "mensagem de eco recebido pelo multicast: ";
+			cerr << m.tipo() << " ";
+			cerr << m.origem() << " ";
+			cerr << m.tamanho() << " ";
+			cerr << m.repeticoes() << endl;
+			dump(data);
+		} else {
+			trata_mensagem(m, origin);
+		}
 	}
+	void trata_mensagem(const Mensagem& m,
+			    ip::udp::endpoint origin) {
+		switch (m.tipo()){
+		case DISPARA:
+		{
+			switch (m.origem()){
+			case SERVIDOR:
+				cerr << "Servidor respondeu (ack): ";
+				dump(m.pack());
+				ack_servidor = true;
+				break;
+			case CLIENTE:
+				cerr << "Cliente respondeu (ack): ";
+				dump(m.pack());
+				ack_cliente = true;
+				break;
+			default:
+				cerr << "origem inválida" << endl;
+				cerr << "Deveria ser inalcançável 🐛" << endl;
+				break;
+			}
+
+			break;
+		}
+		case EXITO:
+			switch (m.origem()){
+			case SERVIDOR:
+				cerr << "Servidor respondeu com êxito: ";
+				dump(m.pack());
+				exito_servidor = true;
+				if (exito_servidor && exito_cliente){
+					Mensagem m(DESLIGA, origem_local, 0x13, 0x37);
+					send_to(socket_, Mensagem::pack(m), mc_endpoint);
+				}
+				break;
+			case CLIENTE:
+				cerr << "Cliente respondeu com êxito: ";
+				dump(m.pack());
+				exito_cliente = true;
+				if (exito_servidor && exito_cliente){
+					cerr << "todo mundo ja foi embora. Tchau!" << endl;
+					socket_.get_io_service().stop();
+				}
+				break;
+			default:
+				cerr << "origem inválida" << endl;
+				cerr << "Deveria ser inalcançável 🐛" << endl;
+				break;
+			}
+			// TODO: Marcar um bit e dar erro caso nao seja marcado?
+			break;
+		case DESLIGA:
+			switch (m.origem()){
+			case SERVIDOR:
+				cerr << "Servidor respondeu reconhecendo o desliga: ";
+				dump(m.pack());
+				desliga_servidor = true;
+				if (desliga_servidor && desliga_cliente){
+					cerr << "todo mundo ja foi embora. Tchau!" << endl;
+					socket_.get_io_service().stop();
+				}
+				break;
+			case CLIENTE:
+				cerr << "Cliente respondeu reconhecendo o desliga: ";
+				dump(m.pack());
+				desliga_servidor = true;
+				if (desliga_servidor && desliga_cliente){
+					cerr << "todo mundo ja foi embora. Tchau!" << endl;
+					socket_.get_io_service().stop();
+				}
+				break;
+			default:
+				cerr << "origem inválida" << endl;
+				cerr << "Deveria ser inalcançável 🐛" << endl;
+				break;
+			}
+			break;
+		default:
+			cerr << "Deveria ser inalcançável 🐛" << endl;
+			break;
+		}
+	}
+
 };
 
 
@@ -63,19 +176,21 @@ int main(int argc, char* argv[])
 	{
 		if (argc != 3)
 		{
-			cerr << "Usage: receiver <listen_address> <multicast_address>\n";
+			cerr << "Usage: orquestrador <multicast_address> <multicast_port>\n";
+			cerr << "       or\n";
+			cerr << "       orquestrador <multicast_address> <multicast_port>\n";
 			cerr << "  For IPv4, try:\n";
-			cerr << "    receiver 239.255.0.1 9900\n";
+			cerr << "    orquestrador 239.255.0.1 9900 127.0.0.1 8800\n";
 			cerr << "  For IPv6, try:\n";
-			cerr << "    receiver ff31::8000:1234 9900\n";
+			cerr << "    orquestrador ff31::8000:1234 9900 127.0.0.1 8800\n";
 			return 1;
 		}
 
 		io_service io_service;
-		Servidor server(io_service,
-				ip::address::from_string(argv[1]),
-				atoi(argv[2]));
-		server.start();
+		Orquestrador orquestrador(io_service,
+					  ip::address::from_string(argv[1]),
+					  atoi(argv[2]));
+		orquestrador.start();
 		io_service.run();
 	}
 	catch (exception& e)
