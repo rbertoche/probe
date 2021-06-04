015e7ee6f70e23c47b7c21658358e1305828720c
rd21 <rd21@VM11.(none)>
2021-06-04 20:17:40 -0300

    Insere gerador de script de teste

---
+++ gen_test_script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def main():
+    f = "dispara {0} {1}\n"
+    repeticoes = 16
+    out = open('test_script.orq', "w")
+    out.writelines((f.format(1 << i, repeticoes)
+		    for i in xrange(2, 31)))
+    out.close()
+
+if __name__ == "__main__":
+    main()
+
+
