51c1ec296bfadce8fd4864c7130e2a740c5470d9
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2021-06-06 11:02:01 -0300

    Adiciona desliga no final do script

--- gen_test_script.py
+++ gen_test_script.py
@@ -3,9 +3,10 @@
 def main():
     f = "dispara {0} {1}\n"
     repeticoes = 16
-    out = open('test_script.orq', "w")
+    out = open("test_script.orq", "w")
     out.writelines((f.format(1 << i, repeticoes)
 		    for i in xrange(2, 31)))
+    out.write("desliga\n")
     out.close()
 
 if __name__ == "__main__":
