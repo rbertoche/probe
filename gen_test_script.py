791a2e5c4e97eb4a00caba3552cc456045b24336
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2021-06-07 19:28:22 -0300

    Parametriza script

--- gen_test_script.py
+++ gen_test_script.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 
+import sys
+
 def main():
+    if len(sys.argv) != 4:
+        print("usage: {0} expoente_tamanho_maximo repeticoes arquivo_de_saida")
+
     f = "dispara {0} {1}\n"
-    repeticoes = 16
-    out = open("test_script.orq", "w")
+    tamanho = int(sys.argv[1]) + 1
+    repeticoes = int(sys.argv[2])
+    out = open(sys.argv[3], "w")
     out.writelines((f.format(1 << i, repeticoes)
-		    for i in xrange(2, 31)))
+		    for i in xrange(2, tamanho)))
     out.write("desliga\n")
     out.close()
 
